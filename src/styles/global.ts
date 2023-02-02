ef8e4a5293f1225083c4f7912f9e408fb79694eb
Erick Marques <erick.marquesc@hotmail.com>
2023-02-01 22:50:33 -0300

    style: :lipstick: add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  };
+  body {
+    background: #071422;
+  };
+  body, input, textarea  {
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 160%;
+    font-family: 'Nunito', sans-serif;
+  };
+`
