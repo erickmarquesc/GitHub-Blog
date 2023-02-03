7a783b91fea5adf56533aa7961804716e50af2c2
Erick Marques <erick.marquesc@hotmail.com>
2023-02-03 17:32:40 -0300

    refactor: :zap: refactor code

--- src/components/CardRepo/index.tsx
+++ src/components/CardRepo/index.tsx
@@ -1,82 +1,66 @@
 import { Container, Content } from "../Containers/styles";
+import { useContextSelector } from "use-context-selector";
+import { RepositoryContext } from "../../context";
+import { formatDistanceToNow } from 'date-fns';
 import { CardRepoContent } from "./styles";
+import ptBR from 'date-fns/locale/pt-BR';
 
 export function CardRepo() {
+
+  const repositories = useContextSelector(RepositoryContext, (context) => {
+    return context.repositories
+  });
+
+  function formatCreatedAt(createdAt: string) {
+    const createdAtFormat = new Date(createdAt);
+
+    const createdAtRelativeToNow = formatDistanceToNow(createdAtFormat, {
+      locale: ptBR,
+      addSuffix: true,
+    });
+
+    return (createdAtRelativeToNow);
+  };
+
   return (
     <Container>
       <Content>
-       
-          <CardRepoContent>
-            <header>
-              <h1>JavaScript data types and data structures</h1>
-              <strong>Há 1 dia</strong>
-            </header>
-            <main>
-              Programming languages all have built-in data structures,
-              but these often differ from one language to another. This
-              article attempts to list the built-in data structures available
-              in JavaScript and what properties they have. These can be used
-              to build other data structures. Wherever possible, comparisons
-              with other languages are drawn.
-            </main>
-          </CardRepoContent>
-          <CardRepoContent>
-            <header>
-              <h1>JavaScript data types and data structures</h1>
-              <strong>Há 1 dia</strong>
-            </header>
-            <main>
-              Programming languages all have built-in data structures,
-              but these often differ from one language to another. This
-              article attempts to list the built-in data structures available
-              in JavaScript and what properties they have. These can be used
-              to build other data structures. Wherever possible, comparisons
-              with other languages are drawn.
-            </main>
-          </CardRepoContent>
-          <CardRepoContent>
-            <header>
-              <h1>JavaScript data types and data structures</h1>
-              <strong>Há 1 dia</strong>
-            </header>
-            <main>
-              Programming languages all have built-in data structures,
-              but these often differ from one language to another. This
-              article attempts to list the built-in data structures available
-              in JavaScript and what properties they have. These can be used
-              to build other data structures. Wherever possible, comparisons
-              with other languages are drawn.
-            </main>
-          </CardRepoContent>
-          <CardRepoContent>
-            <header>
-              <h1>JavaScript data types and data structures</h1>
-              <strong>Há 1 dia</strong>
-            </header>
-            <main>
-              Programming languages all have built-in data structures,
-              but these often differ from one language to another. This
-              article attempts to list the built-in data structures available
-              in JavaScript and what properties they have. These can be used
-              to build other data structures. Wherever possible, comparisons
-              with other languages are drawn.
-            </main>
-          </CardRepoContent>
-          <CardRepoContent>
-            <header>
-              <h1>JavaScript data types and data structures</h1>
-              <strong>Há 1 dia</strong>
-            </header>
-            <main>
-              Programming languages all have built-in data structures,
-              but these often differ from one language to another. This
-              article attempts to list the built-in data structures available
-              in JavaScript and what properties they have. These can be used
-              to build other data structures. Wherever possible, comparisons
-              with other languages are drawn.
-            </main>
-          </CardRepoContent>
-        
+        {repositories.map((repo) => {
+          return (
+            <CardRepoContent key={repo.id}>
+                <a href={repo.html_url} target={"_blank"}>
+                  <header>
+                    <h1>{repo.name}</h1>
+                    <strong>{formatCreatedAt(repo.created_at)}</strong>
+                  </header>
+                  <main>
+                    <p>
+                      {repo.description ? repo.description
+                        : 'Não há descrição para esse repositório!'}
+                    </p>
+                    <label>Linguagem: {repo.language ? repo.language : "Nenhuma Linguaguem referenciada"}</label>
+                  </main>
+                  <footer>
+                    {repo.topics.map((topic, index) => {
+                      return (
+                        <>
+                          {
+                            index <= 3 ?
+                              (
+                                <ul>
+                                  <li>#{topic}</li>
+                                </ul>
+
+                              ) : <></>
+                          }
+                        </>
+                      )
+                    })}
+                  </footer>
+                </a>
+            </CardRepoContent>
+          )
+        })}
       </Content>
     </Container>
   );
--- src/components/CardRepo/styles.ts
+++ src/components/CardRepo/styles.ts
@@ -7,27 +7,78 @@ export const CardRepoContent = styled.div`
   padding: 32px;
   border-radius: 8px;
   background: #112131;
+  border-left: solid 2px #3A536B ;
+
+    a{
+      color: #fff;
+      text-decoration: none;
+    }
+
+    /* link que foi visitado */
+    a:visited {
+      color: none;
+    }
+
+    /* mouse over */
+    a:hover {
+      color: #fff;
+    }
+
+    /* link selecionado */
+    a:active {
+      color: #fff;
+    }
+
+  transition: border-left 1s;
+
+  :hover{
+    cursor: pointer;
+    border-left: solid 2px #C4D4E3 ;
+  }
 
   header{
     display: flex;
     flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
 
     h1{
-      width: 283px;
+      width: 210px;
       font-size: 20px;
     };
 
     strong{
+      color: #7B96B2;
       font-size: 14px;
     };
   };
 
   main{
-    margin-top: 20px;
-    overflow: hidden; // Removendo barra de rolagem
-    display: -webkit-box;
-    -webkit-line-clamp: 4; // Quantidade de linhas
-    text-overflow: ellipsis; // Adicionando "..." ao final
-    -webkit-box-orient: vertical; 
+    margin: 20px 0;
+
+    p{
+      color: #AFC2D4;
+      font-size: 16px;
+      overflow: hidden; // Removendo barra de rolagem
+      display: -webkit-box;
+      -webkit-line-clamp: 4; // Quantidade de linhas
+      text-overflow: ellipsis; // Adicionando "..." ao final
+      -webkit-box-orient: vertical; 
+    }
+    label{
+      color: #7B96B2;
+    }
   };
+
+  footer{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    
+
+    ul{
+      gap: 12px;
+      list-style-type: none;
+    }
+  }
 `;
