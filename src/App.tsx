28f010d166b3cd0351f28f4f6e04ba83b04c88f9
Erick Marques <erick.marquesc@hotmail.com>
2023-02-03 17:34:44 -0300

    refactor: :zap: use context

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { CardRepo } from "./components/CardRepo";
 import { Cover } from "./components/Cover"
 import { Header } from "./components/Header";
+import { RepositoryProvider } from "./context";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
 
   return (
-    <>
+    <RepositoryProvider>
       <GlobalStyle />
       <Cover />
       <Header />
       <CardRepo />
-    </>
+    </RepositoryProvider>
   );
 };
 
