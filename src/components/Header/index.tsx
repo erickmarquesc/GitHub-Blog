03e10ce4a0e4c80eccd3ad770d910d9392cefb8d
Erick Marques <erick.marquesc@hotmail.com>
2023-02-11 16:13:44 -0300

    refactor: :zap: add hook

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,17 @@
 import { IoPeople, IoBusiness, IoArrowRedoCircle, IoLocationSharp } from "react-icons/io5";
-import { RepositoryContext } from "../../context/RepositoryContext";
-import { useContextSelector } from "use-context-selector";
+import { useRepository } from "../../context/useRepository";
 import { Container, Content } from "../Containers/styles";
 import { ButtonModal } from "../Modal/ButtonModal";
 import { HeaderBox, IconsItem } from "./styles";
 import { GoMarkGithub } from "react-icons/go";
 
 export function Header() {
-
-  const user = useContextSelector(RepositoryContext, (context) => {
-    return context.user
-  });
+  const { user} = useRepository();
 
   return (
     <Container variant="inHeader">
       <Content>
         <>
-
           <HeaderBox>
             <img src={user.avatar_url} />
             <div className="InfoConteiner">
@@ -35,28 +30,22 @@ export function Header() {
                   <GoMarkGithub />
                   {user.login}
                 </IconsItem>
-
                 <IconsItem>
                   {user.company ? <IoBusiness />
                     : <IoLocationSharp />}
                   {user.company ? user.company
                     : user.location}
                 </IconsItem>
-
                 <IconsItem>
                   <IoPeople />
                   {user.followers} seguidores
                 </IconsItem>
-
                 <IconsItem>
                   <ButtonModal />
                 </IconsItem>
-
               </footer>
             </div>
           </HeaderBox>
-          
-
         </>
       </Content>
     </Container>
