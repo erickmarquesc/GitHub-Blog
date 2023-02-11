3e3eb282c6205a8163687b4d8a599befd4349318
Erick Marques <erick.marquesc@hotmail.com>
2023-02-11 16:14:34 -0300

    refactor: :zap: add hooks

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,58 +1,47 @@
-import { RepositoryContext } from "../../context/RepositoryContext";
 import { Container, Content, ModalContainer } from "./styles";
-import { useContextSelector } from "use-context-selector";
-import { ModalContext } from "../../context/ModalContext";
+import { useRepository } from "../../context/useRepository";
+import { useModal } from "../../context/useModal";
 import { useState } from "react";
 
 export function Modal() {
 
-  const modalState = useContextSelector(ModalContext, (context) => {
-    return context.stateModal
-  });
-
-  const clouseModal = useContextSelector(ModalContext, (context) => {
-    return context.handleClouseModal
-  });
-
-  const userName = useContextSelector(RepositoryContext, (context) => {
-    return context.handleConfigUserName
-  });
+  const { stateModal, handleClouseModal } = useModal();
+  const { handleConfigUserName} = useRepository();
 
   const [userNameConfig, setUserNameConfig] = useState('');
+  
+  function handleConfirmNewUser(userName : string) {
+    handleConfigUserName(userName);
+    setUserNameConfig('');
+  };
 
   return (
-    <Container display={modalState}>
+    <Container display={stateModal}>
       <Content>
         <ModalContainer >
-
           <h1>Escolha um novo usuário</h1>
           <div className="inputAndButtons">
-
             <input
               type="text"
               value={userNameConfig}
               onChange={event => setUserNameConfig(event.target.value)}
-              placeholder="erickmarquesc"
+              placeholder="GitHub Username"
             ></input>
-
             <div className="sectionButtons">
               <button
                 className="clouseButton"
-                onClick={clouseModal}>
+                onClick={handleClouseModal}>
                 Cancelar
               </button>
-              <button
-                onClick={() => userName(userNameConfig)}
+              <button disabled={userNameConfig === ''}
+                onClick={() => handleConfirmNewUser(userNameConfig)}
               >
                 Concluir
               </button>
             </div>
-            
           </div>
-
         </ModalContainer>
       </Content>
     </Container>
-
-  )
-}
+  );
+};
